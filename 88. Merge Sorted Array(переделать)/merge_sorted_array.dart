7245d91ff1f3219662357bc7c8ba2c3a51f00655
weazzy <gamzatabdullaev2003@gmail.com>
2022-09-29 22:20:14 +0300

    refactoring merge_sorted_array

--- 88. Merge Sorted Array(переделать)/merge_sorted_array.dart
+++ 88. Merge Sorted Array(переделать)/merge_sorted_array.dart
@@ -1,29 +1,28 @@
 class Solution {
   void merge(List<int> nums1, int m, List<int> nums2, int n) {
-    List<int> cur1 = nums1.sublist(0, m);
-    List<int> cur2 = nums2.toList();
-    List<int> res = [];
-    int i = 0;
-    int j = 0;
-    while ((i != m) || (j != n)) {
-      if (i == m) {
-        res.addAll(cur2.sublist(j, n));
-        j = n;
-      } else if (j == n) {
-        res.addAll(cur1.sublist(i, m));
-        i = m;
-      } else {
-        if (cur1[i] <= cur2[j]) {
-          res.add(cur1[i]);
-          i++;
+    m = m - 1;
+    n = n - 1;
+    int length = nums1.length - 1;
+    while (m >= 0 || n >= 0) {
+      if (m < 0 || n < 0) {
+        if (m < 0) {
+          nums1[length] = nums2[n];
+          n--;
         } else {
-          res.add(cur2[j]);
-          j++;
+          nums1[length] = nums1[m];
+          m--;
+        }
+      } else {
+        if (nums1[m] >= nums2[n]) {
+          nums1[length] = nums1[m];
+          m--;
+        } else {
+          nums1[length] = nums2[n];
+          n--;
         }
       }
-    }
-    for (var i = 0; i < res.length; i++) {
-      nums1[i] = res[i];
+
+      length--;
     }
   }
 }
